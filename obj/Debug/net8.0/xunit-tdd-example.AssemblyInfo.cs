43caa19fc4e73e072f31b0137928c35c069b9a17
MgKyaw <mgkyaw20112@gmail.com>
2025-07-30 21:36:21 +0700

    feat: Rename file

--- obj/Debug/net8.0/xunit-tdd-example.AssemblyInfo.cs
+++ obj/Debug/net8.0/xunit-tdd-example.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xunit-tdd-example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e37431b8a7cf71ccdd3db15ff286c7be697d20d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bb050fbe75945444fda554899513b4c31879276")]
 [assembly: System.Reflection.AssemblyProductAttribute("xunit-tdd-example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xunit-tdd-example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
