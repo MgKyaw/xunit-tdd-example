2559846ca7dd7fa31bd55e1cfa3b54bf3dc15cab
MgKyaw <mgkyaw20112@gmail.com>
2025-07-30 21:39:24 +0700

    feat: Update assemblyinfo files

--- obj/Debug/net8.0/xunit-tdd-example.AssemblyInfo.cs
+++ obj/Debug/net8.0/xunit-tdd-example.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xunit-tdd-example")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c12f7167d054ad7dc5fbee9e09fb544a1b9781a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ab4b4b7babab7b04c0d278593266e9173333f9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("xunit-tdd-example")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xunit-tdd-example")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
